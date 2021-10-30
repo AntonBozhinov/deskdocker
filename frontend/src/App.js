90391f7e58e13c0768dd39616da6900025ddcf05
AntonBozhinov <anton.bozhinov@gmail.com>
2021-10-30 23:44:10 +0300

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex">
+      <aside className="h-screen sticky top-0 ">
+
+        Fixed Sidebar
+      </aside>
+
+      <main>
+        Content
+      </main>
+    </div>
+  );
+}
+
+export default App;
